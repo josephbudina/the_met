e979af4805bd880bd8496e08d267c9eef702b902
Joseph Budina <joebud3@gmail.com>
2021-01-07 09:30:32 -0700

    Add exibits test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -6,7 +6,7 @@ require './lib/museum'
 
 class MuseumTest < Minitest::Test
   def setup
-    @g_m = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
     @patron_1 = Patron.new("Bob", 20)
@@ -18,5 +18,13 @@ class MuseumTest < Minitest::Test
 
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals,@dead_sea_scrolls,@imax], @dmns.exhibits
   end
 end
