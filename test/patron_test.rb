7cca1061222fc7eb8006edbfbbce8ec69cb59fe3
Joseph Budina <joebud3@gmail.com>
2021-01-07 09:17:49 -0700

    Add intrests test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -16,4 +16,10 @@ class PatronTest < Minitest::Test
     assert_equal 20, @patron_1.spending_money
     assert_equal [], @patron_1.intrests
   end
+
+  def test_it_can_add_intrests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
+  end
 end
