e96c9039142fe8690f829923596717eea9a901da
Joseph Budina <joebud3@gmail.com>
2021-01-07 09:20:14 -0700

    Add intrest method

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,11 +1,15 @@
 class Patron
   attr_reader :name,
               :spending_money,
-              :intrests
+              :interests
 
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
-    @intrests = []
+    @interests = []
+  end
+
+  def add_interest(interest)
+    @interests << interest
   end
 end
